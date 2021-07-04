6ef3f957ae1d7407288729592757948f397903ff
Matthew Almeida <matthew.almeida572@gmail.com>
2021-07-04 11:38:49 -0300

    Fixing code style in CrossCutting

--- CustomerWebApi.Infrastructure.CrossCutting/Extensions.cs
+++ CustomerWebApi.Infrastructure.CrossCutting/Extensions.cs
@@ -6,31 +6,16 @@ namespace CustomerWebApi.Infrastructure.CrossCutting
 {
     public static class Extensions
     {
-        public static int ToInt(this object obj)
-        {
-            return Convert.ToInt32(obj);
-        }
+        public static int ToInt(this object obj) => Convert.ToInt32(obj);
 
-        public static bool IsNull(this object obj)
-        {
-            return obj == null;
-        }
+        public static bool IsNull(this object obj) => obj == null;
 
-        public static bool IsNotNull(this object obj)
-        {
-            return obj != null;
-        }
-
-        public static bool IsNullOrEmpty(this string inputString)
-        {
-            return string.IsNullOrEmpty(inputString);
-        }
-
-        public static bool IsNotNullOrEmpty(this string input)
-        {
-            return !string.IsNullOrEmpty(input);
-        }
-
+        public static bool IsNotNull(this object obj) => obj != null;
+        
+        public static bool IsNullOrEmpty(this string inputString) => string.IsNullOrEmpty(inputString);
+        
+        public static bool IsNotNullOrEmpty(this string input) => !string.IsNullOrEmpty(input);
+        
         public static Guid GetGuid(this Guid guid) => guid.Equals(Guid.Empty) ? Guid.NewGuid() : guid;
 
         public static void IsValid<T>(this T obj)
